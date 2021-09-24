99239cb3fd5c094d564ac63b5e2ccaa35ba8e7b2
Itona <revilogd1@gmail.com>
2021-09-24 03:57:04 +0200

    Projektdateien hinzufügen.

---
+++ PPv3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPv3_System
+{
+    class PPv3System
+    {
+
+    }
+
+    class PPv3PatternAnalyzer
+    {
+    }
+
+
+   
+}
