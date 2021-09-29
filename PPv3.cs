9d056ae0debb51b89420690901cf129dd50bbd86
Itona <revilogd1@gmail.com>
2021-09-29 21:01:13 +0200

    a

--- PPv3.cs
+++ PPv3.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace PPv3_System
 {
-    class PPv3System
-    {
+   public class PPv3System
+   {
 
-    }
+   }
 
-    class PPv3PatternAnalyzer
+    public class PPv3PatternAnalyzer
     {
     }
 
